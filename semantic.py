01e7c4b4d7d7a3f429b8f5d98b132c2ec4ee39eb
jarratta <123466771+jarratta@users.noreply.github.com>
2023-02-16 10:11:53 +0000

    Update semantic.py

--- semantic.py
+++ semantic.py
@@ -1,27 +1,157 @@
+"""
+Author: Alistair Jarratt
+Student ID: AJ22110004854
+Task: T40
+>> APOLOGIES: FORGOT TO LOAD THE .PY INTO DROPBOX!
+Introduction to natural language processing
+"""
+
+import spacy
+
+# Create a function using the simple laguage model
+def task1_word_similarity_simple():
+
+# use the simple language model
+    nlp = spacy.load('en_core_web_sm')
+
+# Check similarity of the words
+    word1 = nlp('cat')
+    word2 = nlp('monkey')
+    word3 = nlp('banana')
+
+#Print the results
+    print(f'First word: {word1} Second word: {word2} - Similarity: {word1.similarity(word2)}')
+    print(f'First word: {word3} Second word: {word2} - Similarity: {word3.similarity(word2)}')
+    print(f'First word: {word3} Second word: {word1} - Similarity: {word3.similarity(word1)}')
+
+# Create a function using the simple laguage model
+def task1_word_similarity_complex():
+
+# use the complex language model
+    nlp = spacy.load('en_core_web_md')
+
+# Check similarity of the words
+    word1 = nlp('cat')
+    word2 = nlp('monkey')
+    word3 = nlp('banana')
+
+#Print the results
+    print(f'First word: {word1} Second word: {word2} - Similarity: {word1.similarity(word2)}')
+    print(f'First word: {word3} Second word: {word2} - Similarity: {word3.similarity(word2)}')
+    print(f'First word: {word3} Second word: {word1} - Similarity: {word3.similarity(word1)}')
+
+# Create a function to compare words using the simple language model
+def vectors_simple():
+
+# Load the simple language model
+    nlp = spacy.load('en_core_web_sm')
+
+# Create tokens for the words to be compared and print the results
+    tokens = nlp('cat apple monkey banana mouse cheese')
+    for token1 in tokens:
+        for token2 in tokens:
+            print(token1.text, token2.text, token1.similarity(token2))
+
+# Create a function to compare words using the complex language model
+def vectors_complex():
+
+# Load the complex language model
+    nlp = spacy.load('en_core_web_md')
+
+# Create tokens for the words to be compared and print the results
+    tokens = nlp('cat apple monkey banana mouse cheese')
+    for token1 in tokens:
+        for token2 in tokens:
+            print(token1.text, token2.text, token1.similarity(token2))
+
+# Create a function to compare sentances using the simple language model
+def sentance_similarity_simple():
+    
+# Load the simple language model  
+    nlp = spacy.load('en_core_web_sm')
+
+# Define the model sentance to compare 
+    sentance_to_compare = "'Why is my cat on the car"
+
+# Define the sentances to compare in a list
+    sentences = ["where did my dog go",
+    "Hello, their is my car",
+    "I\'ve lost my car in my car",
+    "I\'d like my boat back",
+    "I will name my dog Diana"]
+
+# Create tokens for the words to be compared and print the results
+    model_sentence = nlp(sentance_to_compare)
+
+    for sentence in sentences:
+        similarity = nlp(sentence).similarity(model_sentence)
+        print(sentence + " - ", + similarity)
+
+# Create a function to compare sentances using the complex language model
+def sentance_similarity_complex():
+
+# Load the complex language model      
+    nlp = spacy.load('en_core_web_md')
+
+# Define the model sentance to compare 
+    sentance_to_compare = "'Why is my cat on the car"
+
+# Define the sentances to compare in a list
+    sentences = ["where did my dog go",
+    "Hello, their is my car",
+    "I\'ve lost my car in my car",
+    "I\'d like my boat back",
+    "I will name my dog Diana"]
+
+# Create tokens for the words to be compared and print the results
+    model_sentence = nlp(sentance_to_compare)
+
+    for sentence in sentences:
+        similarity = nlp(sentence).similarity(model_sentence)
+        print(sentence + " - ", + similarity)
+
+# Create a menu function to run the various scenarios
+def menu():
+
+    menu_select =""
+
+    while menu_select != "q":
+
+            output = f'——————————————————[SELECT AN OPTION]—————————————————————\n'
+            output += f'Section a function: \n'
+            output += f'a: Word Similarity: Simple Engine \n'
+            output += f'b: Word Similarity: Complex Engine  \n'
+            output += f'c: Working with vectors: Simple Engine \n'
+            output += f'd: Working with vectors: Complex Engine  \n'
+            output += f'e: Sentenece Similaity: Simple Engine \n'
+            output += f'f: Sentenece Similaity: Complex Engine \n'                
+            output += f'q: QUIT the program \n'        
+            output += f'—————————————————————————————————————————————————————————\n'
+            print (output)
+
+            menu_select = input("Select a function: ").lower()   
+            if menu_select in ('a','b','c','d','e','f','q'):
+                if menu_select == "a":
+                    task1_word_similarity_simple()
+                elif menu_select == "b":
+                    task1_word_similarity_complex()
+                elif menu_select == "c":
+                    vectors_simple()
+                elif menu_select == "d":
+                    vectors_complex()
+                elif menu_select == "e":
+                    sentance_similarity_simple()
+                elif menu_select == "f":
+                    sentance_similarity_complex()
+                elif menu_select == "q":
+                    print("Thank-you learning!")
+                    quit()
+
+menu()
+
+
+
 ########################################_TASK 38_1.1_############################################
-
-# import spacy
-# nlp = spacy.load('en_core_web_md')
-
-# word1 = nlp('cat')
-# word2 = nlp('monkey')
-# word3 = nlp('banana')
-
-# print(word1.similarity(word2))
-# print(word3.similarity(word2))
-# print(word3.similarity(word1))
-
-# import spacy
-# nlp = spacy.load('en_core_web_sm')
-
-# word1 = nlp('cat')
-# word2 = nlp('monkey')
-# word3 = nlp('banana')
-
-# print(word1.similarity(word2))
-# print(word3.similarity(word2))
-# print(word3.similarity(word1))
-
 
 # The similiar language model ran however it created the following warning:
 #
@@ -43,100 +173,6 @@
 # All the word combinations were compared.  Also the resoult was difernet for the one result from the simple model.
 
 ###################################_Task 38_1_2_##################################################
-import spacy
-nlp = spacy.load('en_core_web_sm')
-
-tokens = nlp('cat apple monkey banana mouse cheese')
-for tokens1 in tokens:
-    for token1 in tokens:
-        for token2 in tokens:
-            print(token1.text, token2.text, token1.similarity(token2))
-
-# import spacy
-# nlp = spacy.load('en_core_web_md')
-
-# tokens = nlp('cat apple monkey banana mouse cheese')
-# for tokens1 in tokens:
-#     for token1 in tokens:
-#         for token2 in tokens:
-#             print(token1.text, token2.text, token1.similarity(token2))
-
-# Out put of simple model:
-# cat cat 1.0
-# cat apple 0.7090088129043579
-# cat monkey 0.5971202254295349
-# cat banana 0.631443202495575
-# cat mouse 0.5055392384529114
-# cat cheese 0.22862693667411804
-# apple cat 0.7090088129043579
-# apple apple 1.0
-# apple monkey 0.7693575024604797
-# apple banana 0.7130810022354126
-# apple mouse 0.5927436947822571
-# apple cheese 0.23921111226081848
-# monkey cat 0.5971202254295349
-# monkey apple 0.7693575024604797
-# monkey monkey 1.0
-# monkey banana 0.7109044194221497
-# monkey mouse 0.6988571286201477
-# monkey cheese 0.3524942100048065
-# banana cat 0.631443202495575
-# banana apple 0.7130810022354126
-# banana monkey 0.7109044194221497
-# banana banana 1.0
-# banana mouse 0.7421507239341736
-# banana cheese 0.30435097217559814
-# mouse cat 0.5055392384529114
-# mouse apple 0.5927436947822571
-# mouse monkey 0.6988571286201477
-# mouse banana 0.7421507239341736
-# mouse mouse 1.0
-# mouse cheese 0.37378448247909546
-# cheese cat 0.22862693667411804
-# cheese apple 0.23921111226081848
-# cheese monkey 0.3524942100048065
-# cheese banana 0.30435097217559814
-# cheese mouse 0.37378448247909546
-# cheese cheese 1.0
-
-# Outplut from the complex model:
-# cat cat 1.0
-# cat apple 0.2036806046962738
-# cat monkey 0.5929930210113525
-# cat banana 0.2235882580280304
-# cat mouse 0.5328792929649353
-# cat cheese 0.19478504359722137
-# apple cat 0.2036806046962738
-# apple apple 1.0
-# apple monkey 0.2342509925365448
-# apple banana 0.6646699905395508
-# apple mouse 0.2800682783126831
-# apple cheese 0.533275842666626
-# monkey cat 0.5929930210113525
-# monkey apple 0.2342509925365448
-# monkey monkey 1.0
-# monkey banana 0.4041501581668854
-# monkey mouse 0.48655828833580017
-# monkey cheese 0.1834208071231842
-# banana cat 0.2235882580280304
-# banana apple 0.6646699905395508
-# banana monkey 0.4041501581668854
-# banana banana 1.0
-# banana mouse 0.18919304013252258
-# banana cheese 0.5478538870811462
-# mouse cat 0.5328792929649353
-# mouse apple 0.2800682783126831
-# mouse monkey 0.48655828833580017
-# mouse banana 0.18919304013252258
-# mouse mouse 1.0
-# mouse cheese 0.21466833353042603
-# cheese cat 0.19478504359722137
-# cheese apple 0.533275842666626
-# cheese monkey 0.1834208071231842
-# cheese banana 0.5478538870811462
-# cheese mouse 0.21466833353042603
-# cheese cheese 1.0
-
 
 #  The simple model produced a warning:
 
@@ -165,40 +201,6 @@ for tokens1 in tokens:
 
 
 ###################################_Task 38_1_3_##################################################
-
-# import spacy
-# nlp = spacy.load('en_core_web_sm')
-
-# sentance_to_compare = "'Why is my cat on the car"
-
-# sentences = ["where did my dog go",
-# "Hello, their is my car",
-# "I\'ve lost my car in my car",
-# "I\'d like my boat back",
-# "I will name my dog Diana"]
-
-# model_sentence = nlp(sentance_to_compare)
-
-# for sentence in sentences:
-#     similarity = nlp(sentence).similarity(model_sentence)
-#     print(sentence + " - ", + similarity)
-
-# import spacy
-# nlp = spacy.load('en_core_web_md')
-
-# sentance_to_compare = "'Why is my cat on the car"
-
-# sentences = ["where did my dog go",
-# "Hello, their is my car",
-# "I\'ve lost my car in my car",
-# "I\'d like my boat back",
-# "I will name my dog Diana"]
-
-# model_sentence = nlp(sentance_to_compare)
-
-# for sentence in sentences:
-#     similarity = nlp(sentence).similarity(model_sentence)
-#     print(sentence + " - ", + similarity)
 
 # # Once again the code ran but with the same warning message:
 
@@ -230,7 +232,6 @@ for tokens1 in tokens:
 # I've lost my car in my car -  0.500795732031743
 # I'd like my boat back -  0.2929248722021074 <<<<<<<<Lowest
 # I will name my dog Diana -  0.340654217410769
-
 
 # Complex:
 # where did my dog go -  0.5817438469726575
